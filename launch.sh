3fe37d57255e9f92391b34c3f84555787290d05f
admin <zuevdaniil.zuevdaniil@gmail.com>
2021-12-20 13:43:39 +0700

    windows setup script fixed a bit

--- launch.sh
+++ launch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
+source "env/bin/activate"
 nohup python src/telebot.py > /dev/null 2>&1 &
 echo "launched the bot"
--- setup.bat
+++ setup.bat
@@ -1,8 +1,8 @@
-choco install python3
-python3 -m pip install --user virtualenv
-python3 -m -venv env
+python -m pip install virtualenv
+python -m venv env
 call env/scripts/activate
 pip install -r requirements.txt
 
 mkdir storage
-touch storage/chip.txt
+mkdir private
+
